1b06e555ab88758849fd973b72f2bd98ea344c80
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2019-12-12 13:50:20 +0100

    Update code

--- rescuebot.ino
+++ rescuebot.ino
@@ -1,51 +1,42 @@
-// start http://145.24.238.50/
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include "config.h"
 #include "controls.h"
 
 #define MOTOR_LEFT_FORWARD_PIN D0
-#define MOTOR_LEFT_BACKWARDS_PIN D1
+#define MOTOR_LEFT_BACKWARD_PIN D1
 #define MOTOR_LEFT_ENABLE_PIN D2
 
 #define MOTOR_RIGHT_FORWARD_PIN D3
-#define MOTOR_RIGHT_BACKWARDS_PIN D4
+#define MOTOR_RIGHT_BACKWARD_PIN D4
 #define MOTOR_RIGHT_ENABLE_PIN D5
 
-#define API_MOTOR_STATE_STOP 0
-#define API_MOTOR_STATE_FORWARD 1
-#define API_MOTOR_STATE_LEFT 2
-#define API_MOTOR_STATE_RIGHT 3
-#define API_MOTOR_STATE_BACKWARDS 4
-
+/*#define IR_LEFT_PIN D6
+#define IR_RIGHT_PIN D7
 #define PULSE_SERVO_PIN D8
 
+#define TRIGGER_PIN D9
 #define ECHO_PIN_LOW_MOUNTED 10
 #define ECHO_PIN_HIGH_MOUNTED A0
-#define TRIGGER_PIN 9
-
-#define IR_OUT_RIGHT_PIN D7
-#define IR_OUT_LEFT_PIN D6
-
+*/
 
 void motor_move_forward() {
     digitalWrite(MOTOR_LEFT_FORWARD_PIN, HIGH);
-    digitalWrite(MOTOR_LEFT_BACKWARDS_PIN, LOW);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, LOW);
     digitalWrite(MOTOR_LEFT_ENABLE_PIN, HIGH);
 
     digitalWrite(MOTOR_RIGHT_FORWARD_PIN, HIGH);
-    digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN, LOW);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, LOW);
     digitalWrite(MOTOR_RIGHT_ENABLE_PIN, HIGH);
 }
 
-void motor_move_backwards() {
+void motor_move_backward() {
     digitalWrite(MOTOR_LEFT_FORWARD_PIN, LOW);
-    digitalWrite(MOTOR_LEFT_BACKWARDS_PIN, HIGH);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, HIGH);
     digitalWrite(MOTOR_LEFT_ENABLE_PIN, HIGH);
 
     digitalWrite(MOTOR_RIGHT_FORWARD_PIN, LOW);
-    digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN, HIGH);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, HIGH);
     digitalWrite(MOTOR_RIGHT_ENABLE_PIN, HIGH);
 }
 
@@ -53,13 +44,13 @@ void motor_turn_left() {
     digitalWrite(MOTOR_LEFT_ENABLE_PIN, LOW);
 
     digitalWrite(MOTOR_RIGHT_FORWARD_PIN, HIGH);
-    digitalWrite(MOTOR_RIGHT_BACKWARDS_PIN, LOW);
+    digitalWrite(MOTOR_RIGHT_BACKWARD_PIN, LOW);
     digitalWrite(MOTOR_RIGHT_ENABLE_PIN, HIGH);
 }
 
 void motor_turn_right() {
     digitalWrite(MOTOR_LEFT_FORWARD_PIN, HIGH);
-    digitalWrite(MOTOR_LEFT_BACKWARDS_PIN, LOW);
+    digitalWrite(MOTOR_LEFT_BACKWARD_PIN, LOW);
     digitalWrite(MOTOR_LEFT_ENABLE_PIN, HIGH);
 
     digitalWrite(MOTOR_RIGHT_ENABLE_PIN, LOW);
@@ -76,13 +67,14 @@ void setup() {
     Serial.begin(115200);
 
     pinMode(MOTOR_LEFT_FORWARD_PIN, OUTPUT);
-    pinMode(MOTOR_LEFT_BACKWARDS_PIN, OUTPUT);
+    pinMode(MOTOR_LEFT_BACKWARD_PIN, OUTPUT);
     pinMode(MOTOR_LEFT_ENABLE_PIN, OUTPUT);
 
     pinMode(MOTOR_RIGHT_FORWARD_PIN, OUTPUT);
-    pinMode(MOTOR_RIGHT_BACKWARDS_PIN, OUTPUT);
+    pinMode(MOTOR_RIGHT_BACKWARD_PIN, OUTPUT);
     pinMode(MOTOR_RIGHT_ENABLE_PIN, OUTPUT);
 
+    /*
     pinMode(PULSE_SERVO_PIN, OUTPUT);
 
     pinMode(ECHO_PIN_LOW_MOUNTED, INPUT);
@@ -91,7 +83,7 @@ void setup() {
 
     pinMode(IR_OUT_RIGHT_PIN, INPUT);
     pinMode(IR_OUT_LEFT_PIN, INPUT);
-
+    */
 
     Serial.print("\nConnecting to ");
     Serial.print(wifi_ssid);
@@ -110,22 +102,22 @@ void setup() {
         server.send(200, "text/html", controls_html);
     });
 
-    server.on("/api/update_motor", []() {
-        int32_t motor_state = atoi(server.arg("state").c_str());
+    server.on("/api/update_motor_state", []() {
+        String motor_state = server.arg("motor_state");
 
-        if (motor_state == API_MOTOR_STATE_FORWARD) {
+        if (motor_state == "forward") {
             motor_move_forward();
         }
-        if (motor_state == API_MOTOR_STATE_LEFT) {
+        if (motor_state == "left") {
             motor_turn_left();
         }
-        if (motor_state == API_MOTOR_STATE_RIGHT) {
+        if (motor_state == "right") {
             motor_turn_right();
         }
-        if (motor_state == API_MOTOR_STATE_BACKWARDS) {
-            motor_move_backwards();
+        if (motor_state == "backward") {
+            motor_move_backward();
         }
-        if (motor_state == API_MOTOR_STATE_STOP) {
+        if (motor_state == "stop") {
             motor_stop();
         }
 
